34f91e1590c4222dea7c38d44203fb1c84a85565
param montejano <parammontejano@gmail.com>
2021-01-14 17:16:45 +0100

    Lógica PlantaView botones y pantallas

--- src/main/java/com/practica2/tap/views/planta/PlantaView.java
+++ src/main/java/com/practica2/tap/views/planta/PlantaView.java
@@ -3,6 +3,7 @@ package com.practica2.tap.views.planta;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.practica2.tap.logic.Building;
 import com.practica2.tap.views.main.MainView;
 import com.vaadin.flow.component.Component;
 import com.vaadin.flow.component.button.Button;
@@ -20,15 +21,20 @@ import com.vaadin.flow.router.Route;
 @PageTitle("Plantas")
 public class PlantaView extends VerticalLayout{
 	
+	private Building edificio;
+	
 	public PlantaView() {
+		
+		edificio = Building.getInstance(7, 3);
+		
 		Tabs tabs = new Tabs();
 		Div pages = new Div();
 		Map<Tab, Component> tabsToPages = new HashMap<>();
 		
-		for (int i = 1; i <= 7; i++) {
+		for (int i = 0; i < 7; i++) {
 			Tab tab = new Tab("Planta " + i);
 			Div page = new Div();
-			page.add(plantaLayout());
+			page.add(plantaLayout(i));
 			
 			if(i!=1) page.setVisible(false);
 			
@@ -47,7 +53,7 @@ public class PlantaView extends VerticalLayout{
 		add(tabs, pages);
 	}
 	
-	public Component plantaLayout() {
+	public Component plantaLayout(int planta) {
 		
 		FormLayout plantaLayout = new FormLayout();		
 		plantaLayout.setResponsiveSteps(new ResponsiveStep("40em", 3));
@@ -55,14 +61,33 @@ public class PlantaView extends VerticalLayout{
 		TextField altavoz = new TextField();
 		altavoz.setReadOnly(true);
 		TextField pantallaAscensor1 = new TextField("Planta");
+		pantallaAscensor1.setValue(Integer.toString(edificio.getElevators().get(0).getCurrentFloor()));
 		pantallaAscensor1.setReadOnly(true);
-		Button botonAscensor1 = new Button("Llamar");
+		
 		TextField pantallaAscensor2 = new TextField("Planta");
+		pantallaAscensor2.setValue(Integer.toString(edificio.getElevators().get(1).getCurrentFloor()));
 		pantallaAscensor2.setReadOnly(true);
-		Button botonAscensor2 = new Button("Llamar");
+		
 		TextField pantallaAscensor3 = new TextField("Planta");
-		pantallaAscensor3.setReadOnly(true);
+		pantallaAscensor3.setValue(Integer.toString(edificio.getElevators().get(2).getCurrentFloor()));
+		pantallaAscensor3.setReadOnly(true);				
+		
+		Button botonAscensor1 = new Button("Llamar");
+		Button botonAscensor2 = new Button("Llamar");
 		Button botonAscensor3 = new Button("Llamar");
+		
+		
+		botonAscensor1.addClickListener(e -> {
+			edificio.getElevators().get(0).select_floor(planta);			
+		});
+		
+		botonAscensor2.addClickListener(e -> {
+			edificio.getElevators().get(1).select_floor(planta);
+		});
+		
+		botonAscensor3.addClickListener(e -> {
+			edificio.getElevators().get(2).select_floor(planta);
+		});
 		
 		plantaLayout.add(altavoz, 3);
 		plantaLayout.add(pantallaAscensor1, pantallaAscensor2, pantallaAscensor3);
